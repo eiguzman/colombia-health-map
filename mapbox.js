6c6cdb769235c1bb9515637060e1db4e93443207
eiguzman <eiguzman@ucsd.edu>
2025-06-18 00:33:25 -0700

    Added Mapbox

---
+++ mapbox.js
@@ -0,0 +1,15 @@
+mapboxgl.accessToken = 'pk.eyJ1IjoiZWlndXptYW4iLCJhIjoiY203OGFhbXVoMHN4ajJrb3Z2ZWUxOW01cyJ9.zc52D1EcGb00gJFeUk6SNg';
+
+const svg = d3.select('#mapbox').select('svg');
+const map = new mapboxgl.Map({
+    container: 'mapbox',
+    style: "mapbox://styles/eiguzman/cmc1cdyej009d01soam0y4n8d",
+    center: [-74.083618, 4.653433],
+    zoom: 11,
+    minZoom: 5,
+    maxZoom: 11
+});
+
+map.on('load', () => {
+    map.resize();
+});
